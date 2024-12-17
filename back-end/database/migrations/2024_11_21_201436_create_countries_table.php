ac3e3b83a5bcf6c1c3183775f3291149ea0a41d0
charifa <bounagatcharifa01@gmail.com>
2024-12-18 00:08:48 +0100

    commit changes

--- back-end/database/migrations/2024_11_21_201436_create_countries_table.php
+++ back-end/database/migrations/2024_11_21_201436_create_countries_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('countries', function (Blueprint $table) {
-            $table->id();
             $table->string('code', 3)->primary();
             $table->string('name', 255);
             $table->jsonb('states')->nullable();
